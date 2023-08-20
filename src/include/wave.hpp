0bf35f1bb772f18f2d73ef0dde9d716b589add75
coolguy1842 <coolio4691.gaming@gmail.com>
2023-08-20 17:56:38 +1000

    fix for linux and add .editorconfig and edit readme

--- src/include/wave.hpp
+++ src/include/wave.hpp
@@ -8,31 +8,6 @@
 
 #include <thread>
 
-#if defined(WIN32) || defined(_WIN32) || defined(__WIN32) && !defined(__CYGWIN__)
-
-
-#include <windows.h>
-
-void sleepfunc(__int64 usec) { 
-    HANDLE timer; 
-    LARGE_INTEGER ft; 
-
-    ft.QuadPart = -(10*usec); // Convert to 100 nanosecond interval, negative value indicates relative time
-
-    timer = CreateWaitableTimer(NULL, TRUE, NULL); 
-    SetWaitableTimer(timer, &ft, 0, NULL, NULL, 0); 
-    WaitForSingleObject(timer, INFINITE); 
-    CloseHandle(timer); 
-}
-
-#else
-void sleepfunc(__int64 usec) {
-    usleep(usec);
-}
-
-#endif
-
-
 enum WaveDirection {
     WAVELEFT = 0,
     WAVERIGHT
